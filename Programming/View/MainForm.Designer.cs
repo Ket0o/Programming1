5d0faf439a2e542a846a98d9c28be4d02b29449c
Ket0o <kupregion3@gmail.com>
2022-02-28 19:45:49 +0700

    На главном окне разместите элемент TabControl, на котором должна быть одна вкладка с названием Enums. TabControl разместите во весь размер окна (свойство Dock в значение Fill).

--- Programming/View/MainForm.Designer.cs
+++ Programming/View/MainForm.Designer.cs
@@ -28,19 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Enums_tabPage = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Enums_tabPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Enums_tabPage
+            // 
+            this.Enums_tabPage.Location = new System.Drawing.Point(4, 34);
+            this.Enums_tabPage.Name = "Enums_tabPage";
+            this.Enums_tabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.Enums_tabPage.Size = new System.Drawing.Size(792, 412);
+            this.Enums_tabPage.TabIndex = 0;
+            this.Enums_tabPage.Text = "Enums";
+            this.Enums_tabPage.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
             this.Name = "MainForm";
             this.Text = "Programming Demo";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage Enums_tabPage;
     }
 }
